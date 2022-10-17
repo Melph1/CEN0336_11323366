964d1d2e3894b46113e966cc4b4ccc0f1dcb5ba1
Melph1 <115952797+Melph1@users.noreply.github.com>
2022-10-16 21:27:58 -0300

    Delete bash_.sript.sh

--- bash_.sript.sh
+++
@@ -1,5 +0,0 @@
-pwd #mostra o caminho atual
-ls -l /home #lista os arquivos e diretórios em home com detalhes
-ls -l /home > lista.HOME.txt #pega a lista de arquivos e diretórios em home e redireciona para um arquivo .txt
-date #mostra a data atual no visor
-
